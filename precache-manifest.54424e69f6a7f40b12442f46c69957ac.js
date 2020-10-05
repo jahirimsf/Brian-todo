e0de6096ab561467d2fae1df68cfab90a1bb8072
Jahir <jahir.imsf@gmail.com>
2020-10-05 20:44:17 +0600

    Updates

---
+++ precache-manifest.54424e69f6a7f40b12442f46c69957ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81a4d06aa4e0763f331cc7aa2fd76ee",
+    "url": "/Brian-todo/index.html"
+  },
+  {
+    "revision": "a909d2a889ffb34f87aa",
+    "url": "/Brian-todo/static/css/main.342f9edb.chunk.css"
+  },
+  {
+    "revision": "7313f2b8fc1bfb9e606d",
+    "url": "/Brian-todo/static/js/2.b916beb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Brian-todo/static/js/2.b916beb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a909d2a889ffb34f87aa",
+    "url": "/Brian-todo/static/js/main.ada84f8a.chunk.js"
+  },
+  {
+    "revision": "4480b2dbfc0792becbac",
+    "url": "/Brian-todo/static/js/runtime-main.ca2fbfbf.js"
+  }
+]);
